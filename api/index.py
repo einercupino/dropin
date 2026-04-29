bd4d928ea3e460e59295b42347fae745db3e485e
einercupino <113217951+einercupino@users.noreply.github.com>
2026-04-29 13:16:29 -0400

    Create index.py

---
+++ api/index.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import sys
+import os
+
+# make sure root path is included
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from drop_in import get_events
+
+app = FastAPI()
+
+@app.get("/")
+def root(start_date: str, end_date: str, sport: str = "both"):
+    return {"results": get_events(start_date, end_date, sport)}

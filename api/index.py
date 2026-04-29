36bd35080260fe445b89779b01b8a830c78703f0
einercupino <113217951+einercupino@users.noreply.github.com>
2026-04-29 13:13:45 -0400

    Delete api directory

--- api/index.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from drop_in import get_events
-
-app = FastAPI()
-
-@app.get("/")
-def root(start_date: str, end_date: str, sport: str = "both"):
-    return {"results": get_events(start_date, end_date, sport)}

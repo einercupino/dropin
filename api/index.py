9590ae43dbd86b9143fa9248e2da393563698ad8
einercupino <113217951+einercupino@users.noreply.github.com>
2026-04-29 13:08:24 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,8 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 def root(start_date: str, end_date: str, sport: str = "both"):
-    try:
-        results = get_events(start_date, end_date, sport)
-        return {"results": results}
-    except Exception as e:
-        return {"error": str(e)}
+    return {"results": get_events(start_date, end_date, sport)}
